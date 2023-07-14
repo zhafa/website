079c366a12215705797b8e8fd6df4a7172d1933a
Zhafa Anbiya <zhafaanbiya311@gmail.com>
2023-07-15 01:58:47 +0700

    website

---
+++ script.js
@@ -0,0 +1,32 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Active Navigation Link
+  window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    let currentSection = '';
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+  
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').substring(1) === currentSection) {
+        link.classList.add('active');
+      }
+    });
+  });
+  
